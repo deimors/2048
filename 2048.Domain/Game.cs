c2029a8adf21124ca370140bc3aa92d2164cab50
deimors <deimors@gmail.com>
2018-03-04 10:51:48 -0600

    Extracted MoveEvaluator

--- 2048.Domain/Game.cs
+++ 2048.Domain/Game.cs
@@ -1,5 +1,4 @@
 ﻿using Functional.Maybe;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,6 +7,12 @@ namespace _2048
 	public class Game
 	{
 		private readonly CellValue[,] _cells = new CellValue[4,4];
+		private readonly MoveEvaluator _moveEvaluator;
+
+		public Game()
+		{
+			_moveEvaluator = new MoveEvaluator(pos => this[pos], IsInBounds);
+		}
 
 		public CellValue this[int row, int column]
 		{
@@ -20,7 +25,10 @@ namespace _2048
 			get => _cells[pos.Row, pos.Column];
 			set => _cells[pos.Row, pos.Column] = value;
 		}
-		
+
+		private static bool IsInBounds(Position pos)
+			=> pos.Row >= 0 && pos.Row <= 3 && pos.Column >= 0 && pos.Column <= 3;
+
 		public void Move(Direction direction)
 		{
 			var positions = GetPositionSequenceForMove(direction);
@@ -33,7 +41,7 @@ namespace _2048
 
 		private void MoveNumberInDirection(Position origin, Direction direction) 
 			=> this[origin].Apply(
-				originNumber => FindMoveTarget(origin, direction)
+				originNumber => _moveEvaluator.FindMoveTarget(origin, direction)
 					.Match(
 						target => MoveToTarget(origin, target, originNumber),
 						() => { }
@@ -68,50 +76,5 @@ namespace _2048
 
 			return rows.SelectMany(row => columns.Select(column => new Position(row, column)));
 		}
-		
-		private static Position GetIncrement(Direction direction)
-		{
-			switch (direction)
-			{
-				case Direction.Right: return new Position(0, 1);
-				case Direction.Down: return new Position(1, 0);
-				case Direction.Left: return new Position(0, -1);
-				case Direction.Up: return new Position(-1, 0);
-				default: throw new ArgumentOutOfRangeException(nameof(direction), direction, $"Unknown Direction: {direction}");
-			}
-		}
-
-		private Maybe<Position> FindMoveTarget(Position position, Direction direction)
-			=> this[position].Match(
-				number => FindMoveTarget(number, Project(position, direction).ToArray()),
-				() => Maybe<Position>.Nothing
-			);
-		
-		private Maybe<Position> FindMoveTarget(int startNumber, (Maybe<Position> previous, Position current)[] projection) 
-			=> projection
-				.FirstMaybe(pair => this[pair.current].HasValue)
-				.SelectOrElse(
-					somePair => this[somePair.current].Value == startNumber
-						? somePair.current.ToMaybe()
-						: somePair.previous,
-					() => projection.LastMaybe().Select(pair => pair.current)
-				);
-
-		private static IEnumerable<(Maybe<Position> previous, Position current)> Project(Position start, Direction direction)
-		{
-			var increment = GetIncrement(direction);
-			var current = start + increment;
-			var previous = Maybe<Position>.Nothing;
-
-			while (IsInBounds(current))
-			{
-				yield return (previous, current);
-				previous = current.ToMaybe();
-				current += increment;
-			}
-		}
-
-		private static bool IsInBounds(Position pos)
-			=> pos.Row >= 0 && pos.Row <= 3 && pos.Column >= 0 && pos.Column <= 3;
 	}
 }
---
+++ 2048.Domain/MoveEvaluator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Functional.Maybe;
+
+namespace _2048
+{
+	internal delegate CellValue GetCellValue(Position position);
+
+	internal delegate bool IsInBounds(Position position);
+
+	internal class MoveEvaluator
+	{
+		private readonly GetCellValue _getCellValue;
+		private readonly IsInBounds _isInBounds;
+
+		public MoveEvaluator(GetCellValue getCellValue, IsInBounds isInBounds)
+		{
+			_getCellValue = getCellValue ?? throw new ArgumentNullException(nameof(getCellValue));
+			_isInBounds = isInBounds ?? throw new ArgumentNullException(nameof(isInBounds));
+		}
+		
+		public Maybe<Position> FindMoveTarget(Position position, Direction direction)
+			=> _getCellValue(position).Match(
+				number => FindMoveTarget(number, Project(position, direction).ToArray()),
+				() => Maybe<Position>.Nothing
+			);
+
+		private Maybe<Position> FindMoveTarget(int startNumber, (Maybe<Position> previous, Position current)[] projection) 
+			=> projection
+				.FirstMaybe(pair => _getCellValue(pair.current).HasValue)
+				.SelectOrElse(
+					somePair => _getCellValue(somePair.current).Value == startNumber
+						? somePair.current.ToMaybe()
+						: somePair.previous,
+					() => projection.LastMaybe().Select(pair => pair.current)
+				);
+
+		private IEnumerable<(Maybe<Position> previous, Position current)> Project(Position start, Direction direction)
+		{
+			var increment = GetIncrement(direction);
+			var current = start + increment;
+			var previous = Maybe<Position>.Nothing;
+
+			while (_isInBounds(current))
+			{
+				yield return (previous, current);
+				previous = current.ToMaybe();
+				current += increment;
+			}
+		}
+		
+		private static Position GetIncrement(Direction direction)
+		{
+			switch (direction)
+			{
+				case Direction.Right: return new Position(0, 1);
+				case Direction.Down: return new Position(1, 0);
+				case Direction.Left: return new Position(0, -1);
+				case Direction.Up: return new Position(-1, 0);
+				default: throw new ArgumentOutOfRangeException(nameof(direction), direction, $"Unknown Direction: {direction}");
+			}
+		}
+	}
+}
