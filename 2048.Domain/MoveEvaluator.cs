65203b5931805dba4d5b8a2b0fb4720f713f7fec
deimors <deimors@gmail.com>
2018-03-10 13:53:44 -0600

    Extracted MoveTargetEvaluator out of MoveEvaluator

--- 2048.Domain/MoveEvaluator.cs
+++ 2048.Domain/MoveEvaluator.cs
@@ -13,17 +13,12 @@ namespace _2048
 	{
 		private readonly Board _board;
 
-		private static readonly IReadOnlyDictionary<Direction, Position> MoveIncrements = new Dictionary<Direction, Position>
-		{
-			{ Direction.Right, new Position(0, 1) },
-			{ Direction.Down, new Position(1, 0) },
-			{ Direction.Left, new Position(0, -1)},
-			{ Direction.Up, new Position(-1, 0)}
-		};
-		
+		private readonly MoveTargetEvaluator _targetEvaluator;
+
 		public MoveEvaluator(Board board)
 		{
 			_board = board ?? throw new ArgumentNullException(nameof(board));
+			_targetEvaluator = new MoveTargetEvaluator(board);
 		}
 
 		public IEnumerable<MoveCandidate> GetMoveCandidates(Direction direction)
@@ -50,53 +45,8 @@ namespace _2048
 
 		private MoveCandidate GetMoveCandidate(Position origin, Direction direction)
 			=> _board[origin].Match(
-				number => new MoveCandidate(number, origin, FindMoveTarget(origin, direction)),
+				number => new MoveCandidate(number, origin, _targetEvaluator.FindMoveTarget(origin, direction)),
 				() => new MoveCandidate(0, origin, Maybe<Position>.Nothing)
 			);
-
-		private Maybe<Position> FindMoveTarget(Position position, Direction direction)
-			=> _board[position].Match(
-				number => FindMoveTarget(number, Project(position, direction).ToArray()),
-				() => Maybe<Position>.Nothing
-			);
-
-		private Maybe<Position> FindMoveTarget(int startNumber, PositionPair[] projection) 
-			=> projection
-				.FirstMaybe(pair => !_board[pair.Current].IsEmpty)
-				.SelectOrElse(
-					somePair => _board[somePair.Current].Equals(startNumber)
-						? somePair.Current.ToMaybe()
-						: somePair.Previous,
-					() => projection.LastMaybe().Select(pair => pair.Current)
-				);
-
-		private IEnumerable<PositionPair> Project(Position start, Direction direction)
-		{
-			var increment = GetIncrement(direction);
-			var current = start + increment;
-			var previous = Maybe<Position>.Nothing;
-
-			while (_board.IsInBounds(current))
-			{
-				yield return new PositionPair(previous, current);
-				previous = current.ToMaybe();
-				current += increment;
-			}
-		}
-
-		private static Position GetIncrement(Direction direction)
-			=> MoveIncrements[direction];
-
-		private struct PositionPair
-		{
-			public readonly Maybe<Position> Previous;
-			public readonly Position Current;
-
-			public PositionPair(Maybe<Position> previous, Position current)
-			{
-				Previous = previous;
-				Current = current;
-			}
-		}
 	}
 }
---
+++ 2048.Domain/MoveTargetEvaluator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Functional.Maybe;
+
+namespace _2048
+{
+	public class MoveTargetEvaluator
+	{
+		private static readonly IReadOnlyDictionary<Direction, Position> MoveIncrements = new Dictionary<Direction, Position>
+		{
+			{Direction.Right, new Position(0, 1)},
+			{Direction.Down, new Position(1, 0)},
+			{Direction.Left, new Position(0, -1)},
+			{Direction.Up, new Position(-1, 0)}
+		};
+
+		private readonly Board _board;
+
+		public MoveTargetEvaluator(Board board)
+		{
+			_board = board ?? throw new ArgumentNullException(nameof(board));
+		}
+		
+		public Maybe<Position> FindMoveTarget(Position position, Direction direction)
+			=> _board[position].Match(
+				number => FindMoveTarget(number, BuildPositionPairs(position, direction).ToArray()),
+				() => Maybe<Position>.Nothing
+			);
+		
+		private Maybe<Position> FindMoveTarget(int startNumber, PositionPair[] projection) 
+			=> projection
+				.FirstMaybe(pair => !_board[pair.Current].IsEmpty)
+				.SelectOrElse(
+					somePair => _board[somePair.Current].Equals(startNumber)
+						? somePair.Current.ToMaybe()
+						: somePair.Previous,
+					() => projection.LastMaybe().Select(pair => pair.Current)
+				);
+
+		private IEnumerable<PositionPair> BuildPositionPairs(Position start, Direction direction)
+		{
+			var increment = GetIncrement(direction);
+			var current = start + increment;
+			var previous = Maybe<Position>.Nothing;
+
+			while (_board.IsInBounds(current))
+			{
+				yield return new PositionPair(previous, current);
+				previous = current.ToMaybe();
+				current += increment;
+			}
+		}
+
+		private static Position GetIncrement(Direction direction)
+			=> MoveIncrements[direction];
+
+		private struct PositionPair
+		{
+			public readonly Maybe<Position> Previous;
+			public readonly Position Current;
+
+			public PositionPair(Maybe<Position> previous, Position current)
+			{
+				Previous = previous;
+				Current = current;
+			}
+		}
+	}
+}
